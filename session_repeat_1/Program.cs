f7fa9c7e88c7d8372e113bb2b154061e7212734b
Fuad0010 <fuadft@code.edu.az>
2023-03-04 12:56:10 +0400

    feat:PathConverter

--- session_repeat_1/Program.cs
+++ session_repeat_1/Program.cs
@@ -11,12 +11,19 @@ namespace session_repeat_1
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
-            string schedulePath = @"C:\Users\DEZHURIK\Desktop\C_Sharp-Repeat\sessioasn_repeat_1\Schedule.txt";
-            string schedulePathJson = @"C:\Users\DEZHURIK\Desktop\C_Sharp-Repeat\session_repeat_1\Schedule.json";
-            string schedulePathXml = @"C:\Users\DEZHURIK\Desktop\C_Sharp-Repeat\session_repeat_1\Schedule.xml";
+            string fileName = @"session_repeat_1\Schedule";
+            FileInfo f = new FileInfo(fileName);
+            fileName = f.FullName;
+
+            string schedulePath = fileName;
+            string schedulePathJson = fileName;
+            string schedulePathXml = fileName;
+
+            PathConverter(ref schedulePath, "txt");
+            PathConverter(ref schedulePathJson, "json");
+            PathConverter(ref schedulePathXml, "xml");
 
             List<Session> sessions = new List<Session>();
 
@@ -25,11 +32,34 @@ namespace session_repeat_1
             ToXml(sessions, schedulePathXml);
 
         }
+        static string PathConverter(ref string path, string format)
+        {
+            if (path.Contains("PathArgs"))
+            {
+                int indexOfPath = path.IndexOf("PathArgs");
+                if (indexOfPath >= 0)
+                    path = path.Remove(indexOfPath);
+                path = path + @"session_repeat_1\Schedule." + format;
+            }
+            else if (path.Contains(@"session_repeat_1"))
+            {
+                int indexOfPath = path.IndexOf("session_repeat_1");
+                if (indexOfPath >= 0)
+                    path = path.Remove(indexOfPath);
+                path = path + @"session_repeat_1\Schedule." + format;
+                return path;
+            }
+            else
+            {
+                path = path + @"session_repeat_1\Schedule." + format;
+            }
+            return path;
+        }
         static void TxtConverter(List<Session> sessions, string txtPath)
         {
             if (sessions == null)
             {
-                Console.WriteLine("no arguments");
+                Console.WriteLine("no argument");
                 return;
             }
             else if (!File.Exists(txtPath))
